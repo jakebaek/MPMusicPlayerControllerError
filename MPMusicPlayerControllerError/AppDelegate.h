24db356a57b977aded357ad7f02c97568ebd550d
hokun baek <baekhokun@gmail.com>
2012-03-28 10:25:59 +0900

    Project Create

---
+++ MPMusicPlayerControllerError/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MPMusicPlayerControllerError
+//
+//  Created by hokun baek on 12. 3. 28..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MPMusicPlayerControllerError/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MPMusicPlayerControllerError
+//
+//  Created by hokun baek on 12. 3. 28..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
